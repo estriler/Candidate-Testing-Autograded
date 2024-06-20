8a7197295552793eee527294ca0cc65efceaaa0c
Eric S <estriler@hotmail.com>
2024-06-20 15:08:55 -0500

    added candidate answer grading segment

--- candidate-testing.js
+++ candidate-testing.js
@@ -30,7 +30,11 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+    if(candidateAnswer === correctAnswer){
+      console.log("Correct!!");
+    }else{
+      console.log("Incorrect...");
+    }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
